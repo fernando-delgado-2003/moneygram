4e59f120b26e69fdbf3771c1d371ef34c0d96dd5
fernanbravo124@gmail.com <fernanbravo124@gmail.com>
2022-03-17 16:40:10 -0500

    j

--- assets/js/verify.js
+++ assets/js/verify.js
@@ -2,14 +2,14 @@ import { setIdLink,getIdLink, auth,addUser,updateUser, onAuthStateChanged } from
 import {message} from "./components/components.js";
 alert("antesssss")
 onAuthStateChanged(auth, (user) => {
-	alert("antes")
+	alert("state")
 	if (user) {
 		let params = new URLSearchParams(location.search);
 		let id = params.get('idlink');
 		alert("parametros")
 		getIdLink(user.uid)
 		.then((data)=>{
-			alert("exho")
+			alert("succses")
 			if(id == data.data().idLink){
 				updateUser(user.uid);
 				setIdLink(user.uid, "")
