8eebd8e8a2dddae92574af9aac032cbe6b361922
fernanbravo124@gmail.com <fernanbravo124@gmail.com>
2022-03-17 14:03:18 -0500

    j

--- assets/js/verify.js
+++ assets/js/verify.js
@@ -1,7 +1,8 @@
 import { setIdLink,getIdLink, auth,addUser,updateUser, onAuthStateChanged } from "./firebase/firestore.js"
 import {message} from "./components/components.js";
-
+alert("antesssss")
 onAuthStateChanged(auth, (user) => {
+	alert("antes")
 	if (user) {
 		let params = new URLSearchParams(location.search);
 		let id = params.get('idlink');
